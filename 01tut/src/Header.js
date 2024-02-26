780132c4accdb01a37516fd50b77e1589ff00c71
bungongtoto <kingsley.bungong@ictuniversity.edu.cm>
2024-02-27 00:17:47 +0100

    inline styles

--- 01tut/src/Header.js
+++ 01tut/src/Header.js
@@ -1,11 +1,15 @@
 import React from 'react'
 
 const Header = () => {
-  return (
-    <header>
-        <h1>Groceries List</h1>
-    </header>
-  )
+    const headerStyle = {
+        backgroundColor: 'royalblue',
+        color: '#fff'
+    };
+    return (
+        <header style={headerStyle}>
+            <h1>Groceries List</h1>
+        </header>
+    )
 }
 
 export default Header
