f374992b0f909c7c5e42d3ce17a821447eff83d8
bungongtoto <kingsley.bungong@ictuniversity.edu.cm>
2024-02-27 00:04:43 +0100

    completed lesson 3 componenets

--- 01tut/src/App.js
+++ 01tut/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
 
 function App() {
-  const handleNameChange = () => {
-    const names = ['bungong', 'kingsley', 'ndzi'];
-    const int = Math.floor(Math.random()*3);
-
-    return names[int];
-  }
-
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello {handleNameChange()} !
-        </p>
-        
-      </header>
+      <Header />
+      <Content />
+      <Footer />
     </div>
   );
 }
