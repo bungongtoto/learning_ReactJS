d6e098211f5d6eeac219f58e8c519433e2667ce3
bungongtoto <kingsley.bungong@ictuniversity.edu.cm>
2024-02-26 22:53:31 +0100

    completed lesson 2

--- 01tut/src/App.js
+++ 01tut/src/App.js
@@ -2,21 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const handleNameChange = () => {
+    const names = ['bungong', 'kingsley', 'ndzi'];
+    const int = Math.floor(Math.random()*3);
+
+    return names[int];
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello {handleNameChange()} !
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
