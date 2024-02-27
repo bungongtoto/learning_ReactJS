310544694845b3bf68bacb9f3c6efab58540b2c6
bungongtoto <kingsley.bungong@ictuniversity.edu.cm>
2024-02-27 14:12:53 +0100

    completed lesson 6 usestate

--- 01tut/src/Content.js
+++ 01tut/src/Content.js
@@ -1,33 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Content = () => {
+    const [name, setName] = useState('kingsley');
+    const [count, setCount] = useState(0);
+
     const handleNameChange = () => {
         const names = ['bungong', 'kingsley', 'ndzi'];
         const int = Math.floor(Math.random()*3);
-    
-        return names[int];
+        setName(names[int]);
     }
     
     const handleClick = () => {
-        console.log('You clicked it');
+        setCount(count+1);
+        console.log(count);
     }
 
-    const handleClick2 = (name) => {
-        console.log(`${name} was clicked`);
-    }
-
-    const handleClick3 = (e) => {
-        console.log(e.target.innerText);
+    const handleClick2 = () => {
+        console.log(count);
     }
 
     return (
         <main>
-            <p onDoubleClick={handleClick}>
-                Hello {handleNameChange()}!
+            <p onDoubleClick={handleNameChange}>
+                Hello {name}!
             </p> 
+            <button onClick={handleNameChange}>Change Name</button>
             <button onClick={handleClick}>Click It</button>
-            <button onClick={()=>handleClick2('kingsley')}>Click It</button>
-            <button onClick={(e)=>handleClick3(e)}>Click It</button>   
+            <button onClick={handleClick2}>Click It</button>   
         </main>
     )
 }
