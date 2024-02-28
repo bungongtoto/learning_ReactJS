0bfcf2a1dfc99c213de105eba29aa344e3bc8fd7
bungongtoto <kingsley.bungong@ictuniversity.edu.cm>
2024-02-28 18:52:12 +0100

    complete lesson 10 project challenge

---
+++ 10tut/src/App.js
@@ -0,0 +1,20 @@
+import Square from "./Square";
+import Input from "./Input";
+import { useState } from "react";
+
+function App() {
+  const [colorValue, setColorValue] = useState();
+  return (
+    <div className="App">
+      <Square
+        colorValue={colorValue}
+      />
+      <Input
+        colorValue={colorValue}
+        setColorValue={setColorValue}
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ 10tut/src/Input.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Input = ({colorValue, setColorValue}) => {
+  return (
+    <form onSubmit={(e)=> e.preventDefault()}>
+         <label>Add Color Name:</label>
+            <input
+                autoFocus
+                type="text"
+                placeholder="Add color name"
+                required
+                value={colorValue}
+                onChange={(e) => {
+                    setColorValue(e.target.value);
+                }}
+            />
+    </form>
+  )
+}
+
+export default Input
